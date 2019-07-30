ef873f46e637b652a3b3b255f2f5f07dfb9aa0c8
n30gu1 <n30gu1@icloud.com>
2019-07-30 16:31:37 +0900

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '시험 계산기',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,57 +27,127 @@ class HomePage extends StatelessWidget {
               colors: [Colors.blue, Colors.red])),
         child: Center(
           child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 35, bottom: 10),
-                        child: Text(
-                          '시험 계산기',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 43
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 200,
-                    child: RaisedButton(
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            child: Text(
-                              '평균 계산',
-                              style: TextStyle(
-                                fontSize: 40,
-                                color: Colors.black87
-                              ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, top: 35, bottom: 10),
+                          child: Text(
+                            '시험 계산기',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 43
                             ),
-                            alignment: Alignment(-1, 0.85),
                           ),
-                          Spacer(),
-                          Container(
-                            child: Icon(
-                              Icons.memory,
-                              size: 190,
-                              color: Color.fromRGBO(255, 255, 255, 400.0),
-                            ),
-                          )
-                        ],
-                      ),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                      color: Colors.white70,
-                      onPressed: () { print('button pressed'); },
+                        )
+                      ],
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: double.infinity,
+                      height: 200,
+                      child: RaisedButton(
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                '평균 계산',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.black87
+                                ),
+                              ),
+                              alignment: Alignment(-1, 0.85),
+                            ),
+                            Spacer(),
+                            Container(
+                              child: Icon(
+                                Icons.assignment,
+                                size: 190,
+                                color: Color.fromRGBO(255, 255, 255, 300.0),
+                              ),
+                            )
+                          ],
+                        ),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                        color: Color.fromRGBO(255, 255, 255, 50.0),
+                        onPressed: () { print('button pressed'); },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 200,
+                        child: RaisedButton(
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  '등급 계산',
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      color: Colors.black87
+                                  ),
+                                ),
+                                alignment: Alignment(-1, 0.85),
+                              ),
+                              Spacer(),
+                              Container(
+                                child: Icon(
+                                  Icons.assessment,
+                                  size: 190,
+                                  color: Color.fromRGBO(255, 255, 255, 300.0),
+                                ),
+                              )
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                          color: Color.fromRGBO(255, 255, 255, 50.0),
+                          onPressed: () { print('button pressed'); },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 200,
+                        child: RaisedButton(
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  '크레딧',
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      color: Colors.black87
+                                  ),
+                                ),
+                                alignment: Alignment(-1, 0.85),
+                              ),
+                              Spacer(),
+                              Container(
+                                child: Icon(
+                                  Icons.person,
+                                  size: 190,
+                                  color: Color.fromRGBO(255, 255, 255, 300.0),
+                                ),
+                              )
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                          color: Color.fromRGBO(255, 255, 255, 50.0),
+                          onPressed: () { print('button pressed'); },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
