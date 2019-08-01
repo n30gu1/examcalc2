a06fa6d75be3ec1c48e865cbe185726d4d7f0cc5
n30gu1 <inch224@gmail.com>
2019-08-01 19:50:38 +0900

    clean

---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'avg_calc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '시험 계산기',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blueAccent, Colors.red]
+          )
+        ),
+        child: Center(
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 10, top: 70, bottom: 10),
+                          child: Text(
+                            '시험 계산기',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 43
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 250,
+                      child: RaisedButton(
+                        child: Stack(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  '평균 계산',
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    color: Colors.black87
+                                  ),
+                                ),
+                              )
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Icon(
+                                Icons.assignment,
+                                size: 190,
+                                color: Color.fromRGBO(255, 255, 255, 300.0),
+                              ),
+                            )
+                          ],
+                        ),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                        color: Color.fromRGBO(255, 255, 255, 50.0),
+                        onPressed: () { Navigator.of(context).push(AvgCalcRoute()); },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 250,
+                        child: RaisedButton(
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(bottom: 8),
+                                    child: Text(
+                                      '등급 계산',
+                                      style: TextStyle(
+                                          fontSize: 40,
+                                          color: Colors.black87
+                                      ),
+                                    ),
+                                  )
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Icon(
+                                  Icons.assessment,
+                                  size: 190,
+                                  color: Color.fromRGBO(255, 255, 255, 300.0),
+                                ),
+                              )
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                          color: Color.fromRGBO(255, 255, 255, 50.0),
+                          onPressed: () { Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => AvgCalc())
+                          ); },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 250,
+                        child: RaisedButton(
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(bottom: 8),
+                                    child: Text(
+                                      '크레딧',
+                                      style: TextStyle(
+                                          fontSize: 40,
+                                          color: Colors.black87
+                                      ),
+                                    ),
+                                  )
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Icon(
+                                  Icons.person,
+                                  size: 190,
+                                  color: Color.fromRGBO(255, 255, 255, 300.0),
+                                ),
+                              )
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                          color: Color.fromRGBO(255, 255, 255, 50.0),
+                          onPressed: () { print('button pressed'); },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class AvgCalcRoute extends CupertinoPageRoute {
+  AvgCalcRoute()
+      : super(builder: (BuildContext context) => AvgCalc());
+}
